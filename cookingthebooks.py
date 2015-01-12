32c9e6be33f627a74e79ac46bda3d8635cafe891
Rajath Kumar M.P <rajathkumarmp1995@gmail.com>
2015-01-13 01:12:04 +0530

    sol

--- cookingthebooks.py
+++ cookingthebooks.py
@@ -3,22 +3,19 @@ for _ in range(int(input())):
     a = list(input())
     r = a[:]
     b = a.index(max(a))
-    print(b)
     c = a[0]
     a[0] = max(a)
     a[b] = c
     b = ''.join(a)
     if min(r) < '1':
-        h=min(n for n in r if n!=min(r))
+        h = min(n for n in r if n != min(r))
         x = int(r.index(h))
-        print(x,"yo")
     else:
         x = r.index(min(r))
-        print(x)
     y = r[0]
     r[0] = r[x]
     r[x] = y
     z = ''.join(r)
-    i=1
+    i = 1
     print('Case #', i, ':', ' ', z, ' ', b)
-    i+=1
+    i += 1
